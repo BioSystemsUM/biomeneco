c3067bb6c289ad62008b00936a8caea72d024cba
beach-fossils <jddmoura@gmail.com>
2023-03-10 17:53:51 +0000

    [UPDATE] Minor documentation

--- src/gap_filling_dl/write_sbml/metabolites_to_sbml.py
+++ src/gap_filling_dl/write_sbml/metabolites_to_sbml.py
@@ -1,5 +1,6 @@
 from typing import List, Tuple
-import cobra import Model, Metabolite
+import cobra
+from cobra import Model, Metabolite
 from cobra.io import write_sbml_model
 import os
 
@@ -14,9 +15,9 @@ def write_metabolites_to_sbml(file_name: str, save_path: str, metabolites: List[
     save_path: str
         The path to save the SBML file to.
     metabolites: Tuple[str, str]
-        A list of metabolites to write to the SBML file.
+        A list of metabolites to write to the SBML file, where each metabolite is a tuple of the metabolite ID and the
+        compartment.
     """
-
 
     model = cobra.Model()
     metabolite_cobra_metabolite_objects = []
