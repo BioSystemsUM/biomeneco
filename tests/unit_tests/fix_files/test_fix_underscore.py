2c764bb10655331d359b74bb0da894415b6afc24
beach-fossils <jddmoura@gmail.com>
2023-03-13 17:10:37 +0000

    [ADD] Test the function to transform "__" into "_"
    It passed successfully the test

---
+++ tests/unit_tests/fix_files/test_fix_underscore.py
@@ -0,0 +1,28 @@
+import unittest
+import os
+from tempfile import TemporaryDirectory
+from src.gap_filling_dl.biomeneco.fix_files.fix_underscore import fix_file
+
+
+class TestFixFile(unittest.TestCase):
+    def test_fix_file(self):
+        # Create a temporary directory and file to test on
+        with TemporaryDirectory() as tmpdir:
+            input_file = os.path.join(tmpdir, "test.txt")
+            with open(input_file, "w") as f:
+                f.write("hello__world")
+
+            # Call the function being tested
+            fix_file(input_file)
+
+            # Read the contents of the modified file
+            with open(input_file, "r") as f:
+                result = f.read()
+
+            # Check that the function made the expected changes
+            expected = "hello_world"
+            self.assertEqual(result, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
