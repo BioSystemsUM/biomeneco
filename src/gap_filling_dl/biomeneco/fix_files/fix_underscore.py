b621c344989a8a2b6e1576e94fcf5b9436024c37
beach-fossils <jddmoura@gmail.com>
2023-03-13 17:09:32 +0000

    [ADD] Function to transform "__" into "_"

---
+++ src/gap_filling_dl/biomeneco/fix_files/fix_underscore.py
@@ -0,0 +1,18 @@
+import tempfile
+import sys
+
+
+def fix_file(input_file):
+    with open(input_file, "r") as f:
+        data = f.read()
+
+    # Replace "__" with "_"
+    data = data.replace("__", "_")
+
+    with open(input_file, "w") as f:
+        f.write(data)
+
+
+# try with a file in a path:
+fix_file("/Users/josediogomoura/gap_filling_dl/tests/performance_tests/data/kegg_universal_model.xml")
+
