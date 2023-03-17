ce7d4d2a23777fed69930246aa0bd2c2c14dfe1a
beach-fossils <jddmoura@gmail.com>
2023-03-17 16:47:36 +0000

    [UPDATE] minor testing changes at the to_sbml method
    it passed the test successfuly

--- tests/unit_tests/biomeneco/test_model.py
+++ tests/unit_tests/biomeneco/test_model.py
@@ -2,12 +2,13 @@ import unittest
 import os
 from src.gap_filling_dl.biomeneco.model import Model
 from bioiso import load
+from tests import TEST_DIR
 
 
 class TestModel(unittest.TestCase):
     def setUp(self):
         # load the test model
-        self.model_path = "/Users/josediogomoura/gap_filling_dl/tests/performance_tests/data/models/toy_network.xml"
+        self.model_path = os.path.join(TEST_DIR, "data/models/toy_network.xml")
         self.model = load(self.model_path)
         self.model.objective = "e_Biomass__in"
 
@@ -38,10 +39,37 @@ class TestModel(unittest.TestCase):
             self.assertIsInstance(target[1], str)
 
     def test_to_sbml(self):
-        model = Model(self.model_path)
-        model.to_sbml("test.xml", "tests/performance_tests/data/models")
-        self.assertTrue(os.path.exists("tests/performance_tests/data/models/test.xml"))
-        os.remove("tests/performance_tests/data/models/test.xml")
+        try:
+            model_path = os.path.join(TEST_DIR, "data/models/toy_network.xml")
+            model = Model(model_path)
+            #print(model.model.reactions)
+            model.objective_func = "e_Biomass__in"  # add this line to set the objective function to a string value
+            model.to_sbml("test", "tests/data", seeds=False, targets=False)
+            # as seeds and targets are False by default, the files should not exist, check if they do not exist
+            self.assertFalse(os.path.exists(TEST_DIR + "/data/test_seeds.xml"))  # if the file exists, the test fails
+            self.assertFalse(os.path.exists(TEST_DIR + "/data/test_targets.xml"))  # if the file exists, the test fails,
+            # however if these lines are commented, the test passes, because it adds an extra "=+1" to file_name
+
+            model.identify_seeds()
+            model.identify_targets(objective_function="e_Biomass__in")
+
+            model.to_sbml("test", "/Users/josediogomoura/gap_filling_dl/tests/data", seeds=True, targets=False)
+            #print(os.path.exists(TEST_DIR + "/data/test_seeds.xml"))
+
+            model.to_sbml("test", "/Users/josediogomoura/gap_filling_dl/tests/data", seeds=False, targets=True)
+            #print(os.path.exists(TEST_DIR + "/data/test_targets.xml"))
+
+            #print(os.path.exists(TEST_DIR + "/data/test_seeds.xml"))
+            self.assertTrue(os.path.exists(TEST_DIR + "/data/test_seeds.xml"))
+
+            # if os.path.exists(TEST_DIR + "/data/test_seeds.xml"):
+            #     os.remove(TEST_DIR + "/data/test_seeds.xml")
+            # if os.path.exists(TEST_DIR + "/data/test_targets.xml"):
+            #     os.remove(TEST_DIR + "/data/test_targets.xml")
+
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            raise e
 
 
 if __name__ == "__main__":
