b4cd1800fca1fe8d8cdac18035fc0a1a3456ac59
beach-fossils <jddmoura@gmail.com>
2023-04-04 16:15:53 +0100

    [UPDATE] minor changes; ready to run the with the 6_gaps model

--- tests/performance_tests/cobra_gapfill.py
+++ tests/performance_tests/cobra_gapfill.py
@@ -1,15 +1,23 @@
-from datetime import time
+import os
+import time
 from unittest import TestCase
 from cobra.flux_analysis import gapfill
 from cobra.io import read_sbml_model
+from tests import TEST_DIR
 
 
 class TestCobraGapfill(TestCase):
     def setUp(self) -> None:
-        self.my_model = read_sbml_model('../data/new_toy_test.xml')
-        self.my_model.objective = 'e_Biomass__in'
-        self.universal_model = read_sbml_model('../data/test_model/universal_model_kegg.xml')
+        self.my_model = read_sbml_model(os.path.join(TEST_DIR, "data/6gaps/6gaps_model.xml"))
+        self.my_model.objective = 'Biomass_C3_in'
+        self.universal_model = read_sbml_model(os.path.join(TEST_DIR, "data/6gaps/universal_model_kegg.xml"))
 
     def test_performance_gapfill(self):
+        initial_time = time.time()
         reactions_to_add = gapfill(self.my_model, self.universal_model, demand_reactions=True, exchange_reactions=True)
+        final_time = time.time()
+        elapsed_time = final_time - initial_time
+        print(f"Elapsed time: {elapsed_time} seconds")
         print(reactions_to_add)
+
+
