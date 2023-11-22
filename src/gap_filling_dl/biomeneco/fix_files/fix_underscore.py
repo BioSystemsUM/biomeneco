c6d24fdc0008a69dd2194b8b4e8c9a26d10bbc88
josé diogo moura <97458116+beach-fossils@users.noreply.github.com>
2023-11-22 19:00:15 +0000

    Update fix_underscore.py

--- src/gap_filling_dl/biomeneco/fix_files/fix_underscore.py
+++ src/gap_filling_dl/biomeneco/fix_files/fix_underscore.py
@@ -12,8 +12,3 @@ def fix_file(input_file):
     with open(input_file, "w") as f:
         f.write(data)
 
-
-# try with a file in a path:
-#fix_file("/Users/josediogomoura/gap_filling_dl/tests/performance_tests/data/kegg_universal_model.xml")
-# fix_file("/Users/josediogomoura/gap_filling_dl/tests/data/universal_model_kegg.xml")
-
