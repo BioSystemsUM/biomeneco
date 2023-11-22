2f4c4ccd414bc6aa29038da7bb28af49eab8ec9b
josé diogo moura <97458116+beach-fossils@users.noreply.github.com>
2023-11-22 18:25:03 +0000

    Update utils.py
    write_metabolites_to_sbml added to the file

--- src/gap_filling_dl/biomeneco/utils.py
+++ src/gap_filling_dl/biomeneco/utils.py
@@ -154,3 +154,28 @@ def clone_metabolite(compartments, metabolites):
             cloned_metabolite.compartment = compartment
             cloned_metabolites.append(cloned_metabolite)
     return cloned_metabolites
+    
+
+def write_metabolites_to_sbml(file_name: str, save_path: str, metabolites: List[Tuple[str, str]]):
+    """
+    Write a list of metabolites to an SBML file.
+    Parameters
+    ----------
+    file_name: str
+        The name of the SBML file to write to.
+    save_path: str
+        The path to save the SBML file to.
+    metabolites: Tuple[str, str]
+        A list of metabolites to write to the SBML file, where each metabolite is a tuple of the metabolite ID and the
+        compartment.
+    """
+
+    model = cobra.Model()
+    metabolite_cobra_metabolite_objects = []
+    metabolites = set(metabolites)
+    for metabolite_id, compartment in metabolites:
+        metabolite = Metabolite(id=metabolite_id, compartment=compartment)
+        metabolite_cobra_metabolite_objects.append(metabolite)
+
+    model.add_metabolites(metabolite_cobra_metabolite_objects)
+    write_sbml_model(model, os.path.join(save_path, file_name))
