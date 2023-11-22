e022d7a7f53bf5fbdf54b0cd18d13c38b6ea9f95
josé diogo moura <97458116+beach-fossils@users.noreply.github.com>
2023-11-22 18:59:54 +0000

    Update fix_terms.py

--- src/gap_filling_dl/biomeneco/fix_files/fix_terms.py
+++ src/gap_filling_dl/biomeneco/fix_files/fix_terms.py
@@ -10,15 +10,3 @@ def fix_terms(input_file, output_file):
 
     with open(output_file, 'w') as f:
         f.write(data)
-
-
-# path = '/Users/josediogomoura/gap_filling_dl/tests/data/6gaps/6gaps_model.xml'
-# fix_terms(path, path)
-#
-# path_seeds = '/Users/josediogomoura/gap_filling_dl/tests/data/6gaps/6gaps_seeds.xml'
-# fix_terms(path_seeds, path_seeds)
-#
-# path_targets = '/Users/josediogomoura/gap_filling_dl/tests/data/6gaps/6gaps_targets.xml'
-# fix_terms(path_targets, path_targets)
-
-
